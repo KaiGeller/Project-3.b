8fbd9b86b0d4a8eef61391a98ec137f54da9d897
KaiGeller <gellerka@oregonstate.edu>
2022-04-13 15:08:29 -0700

    1

--- factors.py
+++ factors.py
@@ -2,4 +2,4 @@ integer = int(input("Please enter a positive integer: "))
 print("The factors of " + str(integer) + " are")
 for number in range(integer + 1,0,-1):
     if number > 0 and integer/number %1 == 0:
-        print(str(integer/number))
+        print((integer/number))
